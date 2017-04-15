00eb614d515d9ca9c5ed3ffa6c42dcccef231cb6
aleksandrmatveev <sash6589@gmail.com>
2017-04-15 20:51:24 +0300

    disable test

--- src/test/java/ru/ifmo/fitp/labtesterworker/endpoint/SubmitEndpointTest.java
+++ src/test/java/ru/ifmo/fitp/labtesterworker/endpoint/SubmitEndpointTest.java
@@ -1,43 +1,46 @@
-//package ru.ifmo.fitp.labtesterworker.endpoint;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.converter.HttpMessageConverter;
-//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-//import org.springframework.mock.http.MockHttpOutputMessage;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//import ru.ifmo.fitp.labtesterworker.LabtesterWorkerMain;
-//import ru.ifmo.fitp.labtesterworker.dao.report.SubmitReport;
-//import ru.ifmo.fitp.labtesterworker.service.submit.SubmitService;
-//
-//import java.io.IOException;
-//import java.nio.charset.Charset;
-//import java.util.Arrays;
-//
-//import static org.hamcrest.Matchers.is;
-//import static org.junit.Assert.assertNotNull;
-//import static org.mockito.Matchers.any;
-//import static org.mockito.Mockito.*;
-//import static org.mockito.MockitoAnnotations.initMocks;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = LabtesterWorkerMain.class)
-//@WebAppConfiguration
-//public class SubmitEndpointTest {
-//
+package ru.ifmo.fitp.labtesterworker.endpoint;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.ifmo.fitp.labtesterworker.LabtesterWorkerMain;
+import ru.ifmo.fitp.labtesterworker.dao.report.SubmitReport;
+import ru.ifmo.fitp.labtesterworker.service.submit.SubmitService;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LabtesterWorkerMain.class)
+@WebAppConfiguration
+public class SubmitEndpointTest {
+
 //    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
 //            MediaType.APPLICATION_JSON.getSubtype(),
 //            Charset.forName("utf8"));
@@ -93,4 +96,4 @@
 //
 //        return mockHttpOutputMessage.getBodyAsString();
 //    }
-//}
+}
