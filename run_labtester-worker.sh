e3bf07d9d26a7f931371e04a94cd03c3ff524bd8
Alexander Matveev <am@veeroute.com>
2018-02-07 18:53:56 +0300

    redirect logs to file

--- run_labtester-worker.sh
+++ run_labtester-worker.sh
@@ -5,6 +5,6 @@ rm -rf build
 
 gradle build
 sudo docker build -f Dockerfile -t demo/oracle-java:8 .
-sudo docker run --rm -p 8087:8087 -v $PWD:/build -w /build demo/oracle-java:8 java -jar build/libs/labtester-worker-0.1.0.jar
+sudo docker run --rm -p 8087:8087 -v $PWD:/build -w /build demo/oracle-java:8 java -jar build/libs/labtester-worker-0.1.0.jar >> log.txt &
 
 #java -jar build/libs/labtester-worker-0.1.0.jar >> log.txt &
