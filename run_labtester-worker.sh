c8701e2c3883879b10cc0433e2f9184190d875c5
Alexander Matveev <am@veeroute.com>
2018-02-12 17:31:55 +0300

    change base docker image

--- run_labtester-worker.sh
+++ run_labtester-worker.sh
@@ -4,7 +4,7 @@ rm log.txt
 rm -rf build
 
 gradle build
-sudo docker build -f Dockerfile -t demo/oracle-java:8 .
-sudo docker run --rm -p 8087:8087 -v $PWD:/build -w /build demo/oracle-java:8 java -jar build/libs/labtester-worker-0.1.0.jar >> log.txt &
+sudo docker build -f Dockerfile -t labtester-worker/dockerize:latest .
+sudo docker run --rm -p 8087:8087 labtester-worker/dockerize:latest >> log.txt &
 
 #java -jar build/libs/labtester-worker-0.1.0.jar >> log.txt &
