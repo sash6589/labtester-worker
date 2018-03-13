01581edfd55e1498f4da9b04743f4f2c80eff887
Alexander Matveev <am@veeroute.com>
2018-03-13 19:36:37 +0300

    upload docker image to private registry

--- run_labtester-worker.sh
+++ run_labtester-worker.sh
@@ -12,7 +12,10 @@ rm log.txt
 rm -rf build
 
 gradle build
-sudo docker build -f Dockerfile -t labtester-worker/dockerize:latest .
+
+sudo docker build -f Dockerfile -t labtestersystem/labtester:latest .
+docker login --username labtestersystem --password labtester
+docker push labtestersystem/labtester:latest
 
 curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine
 for i in 1 2 3; do docker-machine create --driver digitalocean --digitalocean-image  ubuntu-16-04-x64 --digitalocean-access-token $1 node-$i; done
@@ -52,8 +55,10 @@ for c in {2..3} ; do
     docker swarm join --token $worker_token $leader_ip:2377
 done
 
-eval $(docker-machine env node-1)
-docker service create -p 80:80 --name webserver nginx
+docker-machine ssh node-1 "
+    docker login --username labtestersystem --password labtester;
+    docker service create -p 8087:8087 --with-registry-auth --name labtester-worker labtestersystem/labtester:latest
+"
 
 
 #sudo docker run --rm -p 8087:8087 labtester-worker/dockerize:latest >> log.txt &
