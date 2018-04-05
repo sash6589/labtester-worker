2f00892bcb7f7858592b29d8ce4df2eb8127b4cd
Alexander Matveev <am@veeroute.com>
2018-04-05 19:51:37 +0300

    modify report building

--- src/main/java/ru/ifmo/fitp/labtesterworker/domain/task/TaskPipeline.java
+++ src/main/java/ru/ifmo/fitp/labtesterworker/domain/task/TaskPipeline.java
@@ -39,13 +39,33 @@ public class TaskPipeline {
     public SubmitReport getReport() {
         SubmitReport report = new SubmitReport();
 
-        report.setTestStdout((String) storage.get(TESTS_RUN_STDOUT_STORAGE_KEY));
-        report.setTestStderr((String) storage.get(TESTS_RUN_STDERR_STORAGE_KEY));
+        StringBuilder result = new StringBuilder();
 
-        report.setCodestyleStdout((String) storage.get(CODESTYLE_STDOUT_STORAGE_KEY));
-        report.setCodestyleStderr((String) storage.get(CODESTYLE_STDERR_STORAGE_KEY));
+        String testStdout = (String) storage.getOrDefault(TESTS_RUN_STDOUT_STORAGE_KEY, "");
+        String testStderr = (String) storage.getOrDefault(TESTS_RUN_STDERR_STORAGE_KEY, "");
 
-        report.setFileTestsResult((String) storage.get(FILE_TESTS_RESULT_STORAGE_KEY));
+        String codestyleStdout = (String) storage.getOrDefault(CODESTYLE_STDOUT_STORAGE_KEY, "");
+        String codestyleStderr = (String) storage.getOrDefault(CODESTYLE_STDERR_STORAGE_KEY, "");
+
+        String fileTestsResult = (String) storage.getOrDefault(FILE_TESTS_RESULT_STORAGE_KEY, "");
+
+        if (!"".equals(testStdout)) {
+            result.append("Tests result:\n").append(testStdout).append("\n").append("\n");
+        }
+        if (!"".equals(testStderr)) {
+            result.append("Tests result:\n").append(testStderr).append("\n").append("\n");
+        }
+        if (!"".equals(codestyleStdout)) {
+            result.append("Codestyle result:\n").append(codestyleStdout).append("\n").append("\n");
+        }
+        if (!"".equals(codestyleStderr)) {
+            result.append(codestyleStderr).append("\n").append("\n");
+        }
+        if (!"".equals(fileTestsResult)) {
+            result.append("Test result:\n").append(fileTestsResult).append("\n").append("\n");
+        }
+
+        report.setFileTestsResult(result.toString());
 
         return report;
     }
